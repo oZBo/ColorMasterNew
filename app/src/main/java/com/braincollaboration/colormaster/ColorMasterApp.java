050ef6826aa203cb7dd597fbeae820bfed15270d
Andreychenko <andreychenko.original@gmail.com>
2016-06-03 16:05:07 +0300

    Add fabric.io integration

--- app/src/main/java/com/braincollaboration/colormaster/ColorMasterApp.java
+++ app/src/main/java/com/braincollaboration/colormaster/ColorMasterApp.java
@@ -3,7 +3,11 @@ package com.braincollaboration.colormaster;
 import android.app.Application;
 import android.graphics.Typeface;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
 import cat.ppicas.customtypeface.CustomTypeface;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by eandreychenko on 27.07.2015.
@@ -12,6 +16,11 @@ public class ColorMasterApp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Answers(), new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
         // Register a Typeface creating first the object, and then registering the object
         // with a name.
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/main_font.ttf");
